b9c8468e103ee68828206f43823454263df31971
yashraj970 <112754137+yashraj970@users.noreply.github.com>
2024-07-13 12:13:06 +0530

    signup page added with the api call

---
+++ client/src/pages/Signup/Signup.js
@@ -0,0 +1,206 @@
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { rootColors } from "../../utilities/Colors/Colors";
+import { notifyError, notifySuccess } from "../../utilities/toast";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
+import { SignUp_Api } from "../../config/constants";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="">
+        Won-Call AI
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const formData = {
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
+      email: data.get("email"),
+      password: data.get("password"),
+      contact: data.get("contact"),
+      emergency: data.get("emergency"),
+      option: data.get("option"),
+    };
+
+    const newErrors = {};
+    if (!formData.firstName.trim()) {
+      newErrors.firstName = "First name is required";
+    }
+    if (!formData.lastName.trim()) {
+      newErrors.lastName = "Last name is required";
+    }
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!/^\S+@\S+\.\S+$/.test(formData.email)) {
+      newErrors.email = "Invalid email address";
+    }
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters long";
+    }
+    setErrors(newErrors);
+    if (Object.keys(newErrors).length === 0) {
+      setLoading(true);
+      axios
+        .post(SignUp_Api, {
+          username: formData?.email,
+          password: formData?.password,
+        })
+        .then((response) => {
+          console.log(response);
+          notifySuccess(response?.data?.message || "Signup successful");
+          setLoading(false);
+          navigate("/login");
+        })
+        .catch((error) => {
+          notifyError("An error occurred while signing up");
+          setLoading(false);
+        });
+    }
+  };
+
+  return (
+    <div style={{ marginTop: "5rem" }}>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            sx={{
+              m: 1,
+              background: rootColors?.primary,
+              fontWeight: "500",
+              color: "white",
+            }}
+          >
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Signup Form
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  error={!!errors.firstName}
+                  helperText={errors.firstName}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  error={!!errors.lastName}
+                  helperText={errors.lastName}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  error={!!errors.email}
+                  helperText={errors.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  error={!!errors.password}
+                  helperText={errors.password}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                backgroundColor: rootColors?.primary,
+                color: "white",
+                "&:hover": {
+                  backgroundColor: rootColors?.primary,
+                },
+                fontWeight: "500",
+              }}
+            >
+              {loading ? <CircularProgress /> : "Sign Up"}
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item xs={12}>
+                <Link href="/login" variant="body2" color={"#fff"}>
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5, color: "#fff", mb: 8 }} />
+      </Container>
+    </div>
+  );
+};
+
+export default Signup;
